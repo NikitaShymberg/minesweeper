7b475478661b41bee1d6b3e3c5d6ccbf9ee11b37
Nikita Shymberg <nikitashymberg@gmail.com>
2019-01-07 16:51:19 -0800

    Added win condition

--- game/board.py
+++ game/board.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append('..') #TODO: could be nicer
 
-from constants import WIDTH, HEIGHT, BOMB
+from constants import WIDTH, HEIGHT, BOMB, MAX_BOMBS
 from random import randint
 from tile import Tile
 
 class Board:
     board = []
+    correctMarks = 0
 
     def __init__(self):
         self.board = [[Tile(0) for _ in range(WIDTH)] for _ in range(HEIGHT)]
@@ -15,7 +16,7 @@ class Board:
     def setupBoard(self):
         setBombs = 0
         # NOTE: might be slow if bomb dense
-        while(setBombs < 10):
+        while(setBombs < MAX_BOMBS):
             row = randint(0, WIDTH-1)
             col = randint(0, HEIGHT-1)
             if(self.board[row][col].value != BOMB):
@@ -119,12 +120,10 @@ class Board:
             string += "\n"
         
         return string
-
-    def markBomb(self, row, col):
-        self.board[row][col].mark()
     
     def explore(self, row, col):
-        if(not self.board[row][col].explored):
+        """ Check the value of a cell """
+        if(not self.board[row][col].explored and not self.board[row][col].marked):
             value = self.board[row][col].explore()
 
             if(value == BOMB):
@@ -133,6 +132,7 @@ class Board:
                 exit()
 
             if(value == 0):
+                # Explore surrounding tiles
                 if self.left(row, col) is not None:
                     if self.left(row, col).value == 0:
                         self.explore(row, col-1)
@@ -156,3 +156,17 @@ class Board:
                         self.explore(row+1, col)
                     elif self.down(row, col).value != BOMB:
                         self.down(row, col).explore()
+    
+    def mark(self, row, col):
+        """ Mark the tile as a bomb and update the number of correctly found bombs """
+        self.board[row][col].mark()
+        if self.board[row][col].marked and self.board[row][col].value == BOMB:
+            self.correctMarks += 1
+
+    def isSolved(self):
+        """ Checks if the only remaining unexplored tiles are marked bombs """
+        if self.correctMarks == MAX_BOMBS:
+            marks = [tile.marked for row in self.board for tile in row]
+            if marks.count(True) == MAX_BOMBS:
+                return True
+        return False
