4cffcbff3d0024eeea790e9aa864067be9b47857
39441 <39441@jisedu.or.id>
2019-01-13 10:30:27 -0800

    Almost done

--- bruteForce/bruteForce.py
+++ bruteForce/bruteForce.py
@@ -8,6 +8,7 @@ from game.tile import Tile
 from game.constants import MAX_BOMBS, BOMB, WIDTH, HEIGHT
 from itertools import permutations, chain
 from sympy.utilities.iterables import multiset_permutations
+from math import factorial
 
 #TESTING
 from pprint import pprint
@@ -37,12 +38,14 @@ class BruteForceSolver:
         return probabilityBoard
     
     def calculateProbabilities(self):
-        """ Calculates the probability of each tile being a bomb """
+        """ Calculates the probability of each tile being a bomb TODO: too long func """
         tilesToConsider = self.getTilesAdjacentToExploredTiles()
         totalUnexplored = self.countUnexploredTiles()
+        noInfoTiles = totalUnexplored - len(tilesToConsider)
 
         print("Number of tiles to consider:", len(tilesToConsider))
         print("Total unexplored tiles:", totalUnexplored)
+        print("Tiles with no info:", noInfoTiles)
 
         possibleBombs = self.permuteBombsInTiles(tilesToConsider)
         exploredTiles = [tile for row in self.board.board for tile in row if tile.explored] 
@@ -50,9 +53,20 @@ class BruteForceSolver:
         # Format: [permutation[tile and isBomb]]
         validBombs = [x for x in possibleBombs if self.isPermutationValid(x, exploredTiles)]
 
+        # Calculate probability of having that many bombs in tilesToConsider
+        bombCounts = [] # bombCounts[i] = the number of bombs in validBombs[i]
+        for v in validBombs:
+            bombCounts.append(len([isBomb for isBomb in v if isBomb['isBomb'] == BOMB]))
+        
+        # The number of bomb permutations given bombs in tilesToConsider
+        permutationsOfOtherTiles = [self.countPermutations(noInfoTiles, self.unmarkedBombs - bc) for bc in bombCounts]
+        print("Permutations of other tiles", permutationsOfOtherTiles) # TODO: testme?
+        
+        print("Number of bombs in each permutation:", bombCounts)
+
         isBombCount = [0] * len(tilesToConsider)
         for i, tile in enumerate(tilesToConsider):
-            for permutation in validBombs:
+            for j, permutation in enumerate(validBombs):
                 for p in permutation:
                     
                     # TESTING
@@ -60,23 +74,24 @@ class BruteForceSolver:
                         print(p['tile'].row, p['tile'].col, p['isBomb'])
                     
                     if p['tile'] == tile and p['isBomb']:
-                        isBombCount[i] += 1 # NOTE I think this is where you weigh it?
+                        isBombCount[i] += permutationsOfOtherTiles[j] # NOTE I think this is where you weigh it?
+                        # Add the number of times that this permutation happened in ALL possible perms
                 # TESTING:
                 if i == 0:
                     print('-'*32)
         print("Number of times this tile was a bomb:", isBombCount)
-        isBombProbability = [count / len(validBombs) for count in isBombCount] # NOTE and here divide by TOTAL possible perms
+        isBombProbability = [count / sum(permutationsOfOtherTiles) for count in isBombCount] # NOTE and here divide by TOTAL possible perms
         print("Probability of this tile being a bomb:", isBombProbability)
         
-        # Calculate probability of having that many bombs in tilesToConsider
-        bombCounts = [] # bombCounts[i] = the number of bombs in validBombs[i]
-        for v in validBombs:
-            bombCounts.append(len([isBomb for isBomb in v if isBomb['isBomb'] == BOMB]))
-        
-        print("Number of bombs in each permutation:", bombCounts)
-        
+        noInfoBombChance = (self.unmarkedBombs - sum(bombCounts)/len(bombCounts)) / noInfoTiles
+        print("Chance of having a bomb in no info tile:", noInfoBombChance)
 
-        return validBombs
+        tilesAndProbability = zip(tilesToConsider, isBombProbability)
+
+        return tilesAndProbability
+    
+    def countPermutations(self, n, r):
+        return factorial(n) / (factorial(n - r))
     
     def isPermutationValid(self, permutation, explored):
         """ Returns true if the given permutation of bombs is valid given the explored tiles """
@@ -171,7 +186,7 @@ class BruteForceSolver:
         return surrounding
     
     def countUnexploredTiles(self):
-        # TODO: speed me up scotty
+        # TODO: speed me up scotty?
         count = 0
         for row in self.board.board:
             for tile in row:
